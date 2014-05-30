53f460daa3fab8f8efe0d0cd022ef2910177cfe4
Badmaul <chris.bellier92@gmail.com>
2014-05-30 23:20:54 +0200

    Create a specific ListMenu to choose a project and session

---
+++ mt4j/CoFiTS/src/utc/bsfile/gui/widget/menu/ProjectChoiceListMenu.java
@@ -0,0 +1,68 @@
+package utc.bsfile.gui.widget.menu;
+
+import org.mt4j.components.TransformSpace;
+import org.mt4j.components.visibleComponents.widgets.buttons.MTImageButton;
+import org.mt4j.input.inputProcessors.IGestureEventListener;
+import org.mt4j.input.inputProcessors.MTGestureEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapEvent;
+import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapProcessor;
+import org.mt4j.util.math.Vector3D;
+
+import processing.core.PApplet;
+import utc.bsfile.model.menu.IMenuModel;
+import utc.bsfile.model.menu.TwoLinkedJsonNode;
+import utc.bsfile.util.ImageManager;
+
+public class ProjectChoiceListMenu extends ListMenu implements ChoiceListener {
+
+	public ProjectChoiceListMenu(PApplet applet, int x, int y, float width,
+			int nbItem, IMenuModel model) {
+		super(applet, x, y, width, nbItem, model);
+		
+		//Add a confirmation button
+		m_confirmButton = new MTImageButton(applet, ImageManager.getInstance().load("confirm-button.png"));
+		
+		m_confirmButton.setPositionRelativeToOther(this, new Vector3D(getWidthXY(TransformSpace.LOCAL) / 2, getHeightXY(TransformSpace.LOCAL) - 25));
+		m_confirmButton.setPickable(true);
+		m_confirmButton.setEnabled(false);
+		
+		
+		m_confirmButton.addGestureListener(TapProcessor.class, new IGestureEventListener() {
+			@Override
+			public boolean processGestureEvent(MTGestureEvent evt) {
+				switch (evt.getId()) {
+				case TapEvent.GESTURE_ENDED :	
+					//TODO
+					//Launch the MTBSScene
+					break;
+				default:
+					break;
+				}
+				return false;
+			}
+		});
+		
+		addChild(m_confirmButton);
+	}
+
+	@Override
+	public void choiceSelected(ChoiceEvent choiceEvent) {
+		m_selectedNode = (TwoLinkedJsonNode) choiceEvent.getChoosedObject();
+		m_confirmButton.setEnabled(true);		
+	}
+
+	@Override
+	public void choiceCancelled(ChoiceEvent choiceEvent) {
+		m_selectedNode = null;
+		m_confirmButton.setEnabled(false);		
+	}
+	
+	
+	public final TwoLinkedJsonNode getSelectedNode(){
+		return m_selectedNode;
+	}
+	
+
+	protected TwoLinkedJsonNode m_selectedNode;
+	protected MTImageButton m_confirmButton;
+}
