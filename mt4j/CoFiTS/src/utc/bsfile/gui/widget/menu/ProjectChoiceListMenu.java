0af60532aee5aed8dc2b46839b685de2b858a082
Badmaul <chris.bellier92@gmail.com>
2014-06-02 00:19:56 +0200

    Add a button to launch the agents

--- mt4j/CoFiTS/src/utc/bsfile/gui/widget/menu/ProjectChoiceListMenu.java
+++ mt4j/CoFiTS/src/utc/bsfile/gui/widget/menu/ProjectChoiceListMenu.java
@@ -9,6 +9,7 @@ import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapProces
 import org.mt4j.util.math.Vector3D;
 
 import processing.core.PApplet;
+import utc.bsfile.main.StartCofitsEntities;
 import utc.bsfile.model.menu.IMenuModel;
 import utc.bsfile.model.menu.TwoLinkedJsonNode;
 import utc.bsfile.util.ImageManager;
@@ -24,11 +25,19 @@ public class ProjectChoiceListMenu extends ListMenu implements ChoiceListener {
 		//Add a confirmation button
 		m_confirmButton = new MTImageButton(applet, ImageManager.getInstance().load("confirm-button.png"));
 		
-		m_confirmButton.setPositionRelativeToOther(this, new Vector3D(getWidthXY(TransformSpace.LOCAL) / 2, getHeightXY(TransformSpace.LOCAL) - 25));
+		m_confirmButton.setPositionGlobal(new Vector3D(getPosition(TransformSpace.GLOBAL).x + getWidthXY(TransformSpace.LOCAL) / 2, getPosition(TransformSpace.GLOBAL).y + getHeightXY(TransformSpace.LOCAL) - 25));
 		m_confirmButton.setPickable(true);
 		m_confirmButton.setEnabled(false);
 		
+		//Add a connexion button
+		m_launchAgentsButton = new MTImageButton(applet, ImageManager.getInstance().load("connect-icon.png"));
+		
+		m_launchAgentsButton.setPositionGlobal(new Vector3D(getPosition(TransformSpace.GLOBAL).x, getPosition(TransformSpace.GLOBAL).y));
+		m_launchAgentsButton.setPickable(true);
+		m_launchAgentsButton.setEnabled(true);
+		
 		addChild(m_confirmButton);
+		addChild(m_launchAgentsButton);
 	}
 
 	@Override
@@ -43,6 +52,7 @@ public class ProjectChoiceListMenu extends ListMenu implements ChoiceListener {
 		m_confirmButton.setEnabled(false);		
 	}
 	
+	//Getters
 	
 	public final TwoLinkedJsonNode getSelectedNode(){
 		return m_selectedNode;
@@ -53,7 +63,11 @@ public class ProjectChoiceListMenu extends ListMenu implements ChoiceListener {
 		return m_confirmButton;
 	}
 	
+	public final MTImageButton getLaunchAgentsButton(){
+		return m_launchAgentsButton;
+	}
 
 	protected TwoLinkedJsonNode m_selectedNode;
 	protected MTImageButton m_confirmButton;
+	protected MTImageButton m_launchAgentsButton;
 }
