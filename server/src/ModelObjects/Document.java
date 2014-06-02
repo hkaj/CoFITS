8b2d784bf0b0fc83f2e746887c6902d76697996e
hkaj <haissam.kaj@gmail.com>
2014-06-03 01:14:26 +0200

    Adding modified_at to Document.

--- server/src/ModelObjects/Document.java
+++ server/src/ModelObjects/Document.java
@@ -18,7 +18,7 @@ public class Document extends ModelObject
 {
 	final Integer id;
 	final String name,type;
-	final static private String[] validProperties = {"name","usedDuring","UsedIn","ownedBy"};
+	final static private String[] validProperties = {"name","usedDuring","UsedIn","ownedBy", "lastModified"};
 
 	@JsonCreator
 	public Document(@JsonProperty("id")Integer id, @JsonProperty("name")String name, @JsonProperty("type")String type)
