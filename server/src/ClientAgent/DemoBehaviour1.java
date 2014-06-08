536ee1ccd46e3f03aef1482d76ffdd00defc3063
hkaj <haissam.kaj@gmail.com>
2014-06-08 18:07:01 +0200

    Add a file sample for demo, update demoBehaviour1

--- server/src/ClientAgent/DemoBehaviour1.java
+++ server/src/ClientAgent/DemoBehaviour1.java
@@ -32,8 +32,8 @@ public final class DemoBehaviour1 extends Behaviour
 
 	int step = 0;
 //	MessageTemplate filter=null;
-	final Session session = new Session("projet sr04",new Timestamp(System.currentTimeMillis()).toString());
-	Document doc = new Document(0,"Architecture",".pdf");
+	final Session session = new Session("projet_ia04_0",new Timestamp(System.currentTimeMillis()).toString());
+	Document doc = new Document(0,"nf11cours-2013",".pdf");
 	AID dest;
 	public DemoBehaviour1()
 	{
@@ -62,7 +62,7 @@ public final class DemoBehaviour1 extends Behaviour
 
 	private void selectDocument()
 	{
-		Predicate[] p = {new Filter("name","Architecture"),new Filter("type",".pdf")};
+		Predicate[] p = {new Filter("name","nf11cours-2013"),new Filter("type",".pdf")};
 		final SelectRequest request = new SelectRequest(objectTypes.document, p);
 		final ACLMessage message = new ACLMessage(ACLMessage.REQUEST);
 		message.addReceiver(this.dest);
@@ -78,7 +78,7 @@ public final class DemoBehaviour1 extends Behaviour
 		ModelObject[] res=null;
 		try
 		{
-//			System.out.println("Documents reçus :"+answer.getContent());
+//			System.out.println("Documents reçus : "+answer.getContent());
 			res = mapper.readValue(answer.getContent(), ModelObject[].class);
 		} catch (IOException e)
 		{
