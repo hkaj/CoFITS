d7e6613b4d19d3b021d7bba2afb229e157b460a5
Badmaul <chris.bellier92@gmail.com>
2014-06-10 11:28:03 +0200

    Add request for asking projects architecture json

---
+++ mt4j/CoFiTS/src/utc/bsfile/model/agent/behaviours/RequestProjectsArchitecture.java
@@ -0,0 +1,51 @@
+package utc.bsfile.model.agent.behaviours;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import utc.bsfile.model.agent.CofitsGuiAgent;
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+
+public class RequestProjectsArchitecture extends OneShotBehaviour {
+
+	public RequestProjectsArchitecture(Agent a) {
+		super(a);
+		m_agent = (CofitsGuiAgent) a;
+	}
+
+	@Override
+	public void action() {
+ACLMessage messageToSend = new ACLMessage(ACLMessage.REQUEST);	//TODO Change the performative or add information
+		
+		//Send to the server
+		messageToSend.addReceiver(m_agent.searchServer());
+		messageToSend.setSender(myAgent.getAID());
+
+		//Set json content
+		ObjectMapper mapper = new ObjectMapper();
+		HashMap<String, String> jsonMap = new HashMap<String, String>();
+		StringWriter stringWriter = new StringWriter();
+		
+		jsonMap.put("action", "LIST");
+		jsonMap.put("login", "TATIN");
+		
+		try {
+			mapper.writeValue(stringWriter, jsonMap);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		messageToSend.setContent(stringWriter.toString());
+		
+		myAgent.send(messageToSend);
+	}
+	
+	//Members
+	private CofitsGuiAgent m_agent;
+
+}
