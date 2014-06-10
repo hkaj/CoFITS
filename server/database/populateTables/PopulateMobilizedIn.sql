3b565f1ce661603be4f5f67cc4ff2cf8eb3fee30
hkaj <haissam.kaj@gmail.com>
2014-06-10 11:50:31 +0200

    #fuckitshipit

--- server/database/populateTables/PopulateMobilizedIn.sql
+++ server/database/populateTables/PopulateMobilizedIn.sql
@@ -4,31 +4,31 @@ INSERT INTO MobilizedIn(document, session)
 	FROM Documents WHERE name = 'terminaux minitel'
 );
 
-INSERT INTO MobilizedIn(document, session_proj, session_date)
+INSERT INTO MobilizedIn(document, session)
 (
 	SELECT id, 2
 	FROM Documents WHERE name = 'rapport écosystèmes numériques'
 );
 
-INSERT INTO MobilizedIn(document, session_proj, session_date)
+INSERT INTO MobilizedIn(document, session)
 (
 	SELECT id, 4
 	FROM Documents WHERE name = 'schema opml'
 );
 
-INSERT INTO MobilizedIn(document, session_proj, session_date)
+INSERT INTO MobilizedIn(document, session)
 (
 	SELECT id, 5
 	FROM Documents WHERE name = 'publication RFID'
 );
 
-INSERT INTO MobilizedIn(document, session_proj, session_date)
+INSERT INTO MobilizedIn(document, session)
 (
 	SELECT id, 6
 	FROM Documents WHERE name = 'publication RFID'
 );
 
-INSERT INTO MobilizedIn(document, session_proj, session_date)
+INSERT INTO MobilizedIn(document, session)
 (
 	SELECT id, 6
 	FROM Documents WHERE name = 'whitepaper tags RFID'
